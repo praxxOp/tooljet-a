1c6f2d58be5f3a409e3cd62686340fcab96497bc
Prathamesh <shelke.prathamesh0012@gmail.com>
2025-07-18 23:41:52 +0530

    new file:   tooljet-a/.gitignore 	new file:   tooljet-a/README.md 	new file:   tooljet-a/eslint.config.js 	new file:   tooljet-a/index.html 	new file:   tooljet-a/package-lock.json 	new file:   tooljet-a/package.json 	new file:   tooljet-a/public/Everflow-logo.svg 	new file:   tooljet-a/public/vite.svg 	new file:   tooljet-a/src/App.css 	new file:   tooljet-a/src/App.jsx 	new file:   tooljet-a/src/components/Navbar/Navbar.css 	new file:   tooljet-a/src/components/Navbar/Navbar.jsx 	new file:   tooljet-a/src/components/input/input.css 	new file:   tooljet-a/src/components/input/input.jsx 	new file:   tooljet-a/src/index.css 	new file:   tooljet-a/src/main.jsx 	new file:   tooljet-a/vite.config.js

---
+++ tooljet-a/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar.jsx";
+
+const App = () => {
+  return (
+    <>
+      <div className="Home-main">
+        <Navbar />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ tooljet-a/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="navbar-main">
+      <div className="navbar-left">
+        <div className="navbar-left-group1">
+          <img src="/Everflow-logo.svg" alt="Everflow-logo" className="navbar-logo" />
+          <h5 className="navbar-title">EverFlow</h5>
+        </div>
+        <div className="navbar-left-group2">
+          <div className="navbar-link">Platform <span className="navbar-caret">▼</span></div>
+          <div className="navbar-link">Product <span className="navbar-caret">▼</span></div>
+          <div className="navbar-link">Pricing</div>
+          <div className="navbar-link">Company <span className="navbar-caret">▼</span></div>
+          <div className="navbar-link">Resources <span className="navbar-caret">▼</span></div>
+        </div>
+      </div>
+      <div className="navbar-right">
+        <div className="navbar-login">Login</div>
+        <button className="navbar-cta">
+          Try for free <span className="navbar-cta-arrow">↗</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
