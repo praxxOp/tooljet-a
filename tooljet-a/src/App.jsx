e8cc6698a997ebb15c16b2b8e8f4fcbd0e676970
Prathamesh <shelke.prathamesh0012@gmail.com>
2025-07-19 00:10:32 +0530

    new file:   tooljet-a/public/bottom-line.svg 	modified:   tooljet-a/src/App.jsx 	modified:   tooljet-a/src/components/input/input.css 	modified:   tooljet-a/src/components/input/input.jsx

--- tooljet-a/src/App.jsx
+++ tooljet-a/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar.jsx";
+import Input from "./components/input/input.jsx";
 
 const App = () => {
   return (
     <>
       <div className="Home-main">
         <Navbar />
+        <Input/>
       </div>
     </>
   );
--- tooljet-a/src/components/input/input.jsx
+++ tooljet-a/src/components/input/input.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
+import './input.css'
 
-const input = () => {
+const Input = () => {
   return (
-    <div>input</div>
+    <div className="main-input">
+      <div className="input-heading-row">
+        <span className="input-heading-highlight">
+          Lorem ipsum{' '}
+          <span className="input-heading-underline-wrapper">
+            dolor sit
+            <img src="/bottom-line.svg" alt="underline" className="input-heading-underline-img" />
+          </span>
+        </span>
+        <div className="input-heading-secondary">in tempor</div>
+      </div>
+      <div className="input-subheading">Lorem ipsum dolor sit amet consectetur adipiscing sed do eiusmod</div>
+      <div className="input-textarea-wrapper">
+        <textarea className="input-textarea" placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" />
+        <span className="input-tab-hint">→Tab</span>
+      </div>
+      <div className="input-features-row">
+        <div className="input-feature">
+          <span className="input-feature-icon">✔️</span>
+          <div>
+            <div className="input-feature-title">Lorem ipsum dolor</div>
+            <div className="input-feature-desc">sit met consectetur</div>
+          </div>
+        </div>
+        <div className="input-feature">
+          <span className="input-feature-icon">✔️</span>
+          <div>
+            <div className="input-feature-title">Lorem ipsum dolor</div>
+            <div className="input-feature-desc">sit met consectetur</div>
+          </div>
+        </div>
+        <div className="input-feature">
+          <span className="input-feature-icon">✔️</span>
+          <div>
+            <div className="input-feature-title">Lorem ipsum dolor</div>
+            <div className="input-feature-desc">sit met consectetur</div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default input
+export default Input
